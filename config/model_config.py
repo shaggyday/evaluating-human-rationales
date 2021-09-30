b97778979691ac30ec7a078c429a95a4d5ceb3c8
Harry Tian <tianh@carleton.edu>
2021-09-30 18:19:44 -0500

    lstm

--- config/model_config.py
+++ config/model_config.py
@@ -2,7 +2,7 @@ from model.roberta_classifier import RobertaClassifier
 from model.lstm_classifier import LSTMClassifier
 from model.sklearn_classifier import RandomForestSKLearnClassifier, LogisticRegressionSKLearnClassifier
 
-model_dict = {'model': ["roberta","lstm"]}
+model_dict = {'model': ["lstm"]}
 # model_dict = {'model': ["random_forest","logistic_regression"]}
 # model_dict = {'model': ["roberta"]}
 
