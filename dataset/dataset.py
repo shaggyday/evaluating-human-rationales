a0c4dafecf4191169a9d98410166b5588a999077
Harry Tian <tianh@carleton.edu>
2021-10-04 12:51:56 -0500

    1

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -183,6 +183,7 @@ def create_test_dataloader(model,
 	# 	data_df['rationale'] = data_df["rationale"].apply(lambda r: occlude_rationale(r,rate=rationale_occlusion_rate))
 
 	data_df['text']= data_df['text'].apply(lambda t:t.replace('[SEP]',model.tokenizer.sep_token))
+	data_df['query']= data_df['query'].apply(lambda t:t.replace('[SEP]',model.tokenizer.sep_token))
 	
 	data_df["sufficiency_text"] = data_df[
 		["text", "rationale"]].apply(lambda s: reduce_by_alpha(*s, fidelity_type="sufficiency"), axis=1)
