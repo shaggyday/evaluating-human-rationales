b4068c72c13b865c23fd002403b7df06fdba5911
Harry Tian <tianh@carleton.edu>
2021-10-08 18:33:17 -0500

    1

--- config/data_config.py
+++ config/data_config.py
@@ -3,14 +3,14 @@ TRAIN_FLAG = True
 CACHING_FLAG = True
 EPOCH_LEVEL_CACHING = False
 
-CREATE_FIDELITY_CURVES = True
+CREATE_FIDELITY_CURVES = False
 NUM_FIDELITY_CURVE_SAMPLES = 1
 
 FIDELITY_OCCLUSION_RATES = [x / 20 for x in range(0, 21)]
 
 OUTPUT_DIR = "../output"
 
-dataset_dict = {'dataset': ["movies"]}
+dataset_dict = {'dataset': ["scifact"]}
 
 dataset_info = {
 	'boolq': {
