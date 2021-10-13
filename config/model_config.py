fb5067a6be222bb5d162cd8a77c596083f3cdc51
shaggyday <tianyz.harry2014@outlook.com>
2021-10-13 13:08:36 -0500

    1

--- config/model_config.py
+++ config/model_config.py
@@ -8,7 +8,8 @@ from model.sklearn_classifier import RandomForestSKLearnClassifier, LogisticRegr
 
 # model_dict = {'model': ["lstm"]}
 # model_dict = {'model': ["random_forest", "logistic_regression"]}
-model_dict = {'model': ["bert"]}
+# model_dict = {'model': ["bert"]}
+model_dict = {'model': ["roberta"]}
 # model_dict = {'model': ["longformer"]}
 
 model_info = {
